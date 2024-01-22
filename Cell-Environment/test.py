7f574070b3cf5d394cfd4a3fe3f7919982e15c48
txx4986 <txx4986@gmail.com>
2024-01-22 18:19:30 +0000

    trained dqn model

--- Cell-Environment/test.py
+++ Cell-Environment/test.py
@@ -1,31 +1,32 @@
-from pettingzoo.test import api_test, parallel_api_test, seed_test, parallel_seed_test, max_cycles_test, render_test, performance_benchmark, test_save_obs
-from cell_env import cell_env
-
-
-# API test
-env = cell_env.env()
-api_test(env)
-
-# Parallel API test
-env = cell_env.parallel_env()
-parallel_api_test(env)
-
-# Seed test
-env_fn = cell_env.env
-seed_test(env_fn)
-
-parallel_env_fn = cell_env.parallel_env
-parallel_seed_test(parallel_env_fn)
-
-# Max Cycles test
-max_cycles_test(cell_env)
-
-# render test
-render_test(env_fn)
-
-# performance benchmark test
-env = cell_env.env()
-performance_benchmark(env)
-
-# save observation test
-test_save_obs(env)
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "RI_venv",
+   "language": "python",
+   "name": "python3"
+  },
+  "language_info": {
+   "codemirror_mode": {
+    "name": "ipython",
+    "version": 3
+   },
+   "file_extension": ".py",
+   "mimetype": "text/x-python",
+   "name": "python",
+   "nbconvert_exporter": "python",
+   "pygments_lexer": "ipython3",
+   "version": "3.10.11"
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 2
+}
